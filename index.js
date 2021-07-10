01c3375bafee496a32bf85009881ca085f05b13f
Linh Tran <linh@xseed.tech>
2021-07-10 06:40:06 +0000

    Shit. How bad it is I just found out that ProjFS is very restrictive, e.g doesnt allow write requests. Project abortedgit status  A new project on virtual file system using WinFSP will be the substitute

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-var NativeExtension = require('bindings')('NativeExtension');
+var nodeProj = require('bindings')('NativeExtension');
+
+// create a virtual file system
+virtFS = nodeProj({
+    root: "C://Program Files/Steam/steamapp/common/...",
+})
+
+// signup for each callback
+virtFS.GetDirectoryEnumerationCallback = function(){
+    
+}
+virtFS.GetFileDataCallback = function(infoObj, byteOffset, length){
+    infoObj.filePathName
+    infoObj.dataStreamId 
+    virtFS.writeFileData(dataStreamId, Buffer.from("1234"));
+}
+
+//start virtualizing
+virtFS.start()
+
+
+
+// Future work
+// nodeProj.pipeFileStream(dataStreamId, ReadableStream stream)
+
 module.exports = NativeExtension;
